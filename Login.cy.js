24fba551c5f555f24e6fb0dfc5b34db8470278a1
sheshaa45 <sheshaa@45>
2025-06-09 16:42:59 +0530

    Logout test cases added

--- Login.cy.js
+++ Login.cy.js
@@ -13,4 +13,10 @@ it('should show error on invalid login', () => {
   cy.get('#login').click();
   cy.contains('Invalid credentials').should('be.visible');
 });
-
+it('should show error on invalid login', () => {
+  cy.visit('https://example.com/login');
+  cy.get('#username').type('wrong');
+  cy.get('#password').type('wrongpass');
+  cy.get('#login').click();
+  cy.contains('Invalid credentials').should('be.visible');
+});
