bd2df667e2b19fe2630f5c6ee991691f8c9413ee
sheshaa45 <sheshaa@45>
2025-06-09 16:22:40 +0530

    Revert "Reapply "Invalid login test added For login test"
    This reverts commit cb8f71a5c2919425fa13798f2bb8e655c6e2926c.

--- Login.cy.js
+++ Login.cy.js
@@ -6,11 +6,3 @@ describe('Login', () => {
     cy.get('#login').click();
   });
 });
-it('should show error on invalid login', () => {
-  cy.visit('https://example.com/login');
-  cy.get('#username').type('wrong');
-  cy.get('#password').type('wrongpass');
-  cy.get('#login').click();
-  cy.contains('Invalid credentials').should('be.visible');
-});
-
